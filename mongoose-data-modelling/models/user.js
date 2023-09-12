61f6800c8f93f0121893da3699f86381cdb0cbcc
Anjali211003 <119666988+Anjali211003@users.noreply.github.com>
2023-09-12 12:48:28 +0530

    Create user.js

---
+++ mongoose-data-modelling/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+//user schema - one-one
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    posts:Array,
+      
+},
+    
+  
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+//find the total post created by this user
+userSchema.virtual("postCount").get(function () {
+  return this.posts.length;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
